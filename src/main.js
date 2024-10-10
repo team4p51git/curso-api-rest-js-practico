c2c51418393c63737896ace6b71fcd6504d6eccc
team4p51git <marthaalba42@gmail.com>
2024-10-09 19:25:06 -0500

    configuración entorno de desarrollo

---
+++ src/main.js
@@ -0,0 +1,4 @@
+//En la sección console al hacer clic en inspeccionar puedo ver
+//el mensaje que imprimo en console.log y corroborar que se está llamando ok
+//el archivo js desde el html 
+console.log('hello');
